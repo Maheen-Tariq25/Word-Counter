2d20c2efacc6b1ce9f106763103f31f08026eb9e
Maheen Tariq <164564115+Maheen-Tariq25@users.noreply.github.com>
2024-05-13 22:19:32 +0500

    My first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let user = await inquirer.prompt({
+    name: "ans",
+    type: "input",
+    message: "Enter your sentence/paragraph"
+});
+let wordCount = user.ans.trim().split(" ").length;
+console.log(`Your sentence/paragraph contains ${wordCount} words`);
